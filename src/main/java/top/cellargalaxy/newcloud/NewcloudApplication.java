a2c2f6d300b341577193a5055f71da0160e9a482
cellargalaxy <cellargalaxy@gmail.com>
2018-07-12 18:41:45 +0800

    备份

--- src/main/java/top/cellargalaxy/newcloud/NewcloudApplication.java
+++ src/main/java/top/cellargalaxy/newcloud/NewcloudApplication.java
@@ -2,7 +2,9 @@ package top.cellargalaxy.newcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching // 启动缓存
 @SpringBootApplication
 public class NewcloudApplication {
 
