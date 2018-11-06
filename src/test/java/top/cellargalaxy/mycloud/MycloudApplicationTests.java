48d796b0a1ecbede8345a0d0396630f8141d1208
cellargalaxy <cellargalaxy@gmail.com>
2018-11-06 14:36:13 +0800

    备份

--- src/test/java/top/cellargalaxy/mycloud/MycloudApplicationTests.java
+++ src/test/java/top/cellargalaxy/mycloud/MycloudApplicationTests.java
@@ -10,9 +10,12 @@ import top.cellargalaxy.mycloud.model.bo.OwnBo;
 import top.cellargalaxy.mycloud.service.OwnService;
 import top.cellargalaxy.mycloud.util.IOUtil;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -26,12 +29,36 @@ public class MycloudApplicationTests {
 	public void test() throws Exception {
 	}
 
+	private static void read() throws IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		File file = new File("d:/baifen.json");
+
+		if (!file.exists()) {
+			return;
+		}
+		LinkedList<OwnBo> ownBos=null;
+		try (BufferedReader reader = IOUtil.getReader(file)) {
+			StringBuilder stringBuilder = new StringBuilder();
+			String string;
+			if ((string = reader.readLine()) != null) {
+				stringBuilder.append(string);
+			}
+			ownBos= new LinkedList<>(Arrays.asList(objectMapper.readValue(stringBuilder.toString(), OwnBo[].class)));
+		}
+		Collections.reverse(ownBos);
+
+		try (Writer writer = IOUtil.getWriter(file)) {
+			writer.write(objectMapper.writeValueAsString(ownBos));
+		}
+	}
+
 	private void write(OwnService ownService) throws IOException {
 		ObjectMapper objectMapper = new ObjectMapper();
 		File file = new File("d:/baifen.json");
 
 		List<OwnBo> list = ownService.listAllOwn();
 		LinkedList<OwnBo> ownBos = new LinkedList<>(list);
+		Collections.reverse(ownBos);
 
 		try (Writer writer = IOUtil.getWriter(file)) {
 			writer.write(objectMapper.writeValueAsString(ownBos));
